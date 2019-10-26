81e4725ae770fc56ebb977367d3be05db70d6ed9
cwervo <acwervo@gmail.com>
2019-10-26 01:16:39 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# BSQR
+
+Simple little script that runs [`browser-sync`](https://www.browsersync.io/) & displays a QR code with the external URL.
+
+# Installation
+
+To run once-in-a-while use:
+
+```
+npx bsqr
+```
+
+If you'll need it a lot install it locally with:
+
+``` bash
+npm install -g bsqr
+# or
+yarn global add bsqr
+```
---
+++ index.js
@@ -0,0 +1,33 @@
+let qrcode = require('qrcode-terminal');
+let bs = require("browser-sync").create();
+let devip = require('dev-ip');
+console.log()
+
+const commander = require('commander');
+
+const program = new commander.Command();
+
+program
+    .option('-p, --port <number>', 'port', 3000)
+  .option('-ns, --no-server', 'Stop the server from running')
+  .option('-s, --server <path>', 'Run a Local server (uses your cwd as the web root) [BrowserSync Option]', '.')
+  .option('-sm, --small', 'output extra debugging')
+
+program.parse(process.argv)
+console.log(program.opts());
+
+// .init starts the server
+let browserSyncOptions = {}
+if (program.noServer == undefined) {
+    browserSyncOptions
+        .server = (program.server == true) ? '.' : program.server
+}
+
+bs.init(browserSyncOptions)
+
+// qrcode.setErrorLevel('Q');
+const externalURL = `http://${devip()[0]}:${program.port}`
+qrcode.generate(externalURL, {small: true});
+console.log(externalURL)
+
+bs.reload("*");
