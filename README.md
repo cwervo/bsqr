6816611d8352d214e6d6a72b9e43eb00b9c3c05a
cwervo <acwervo@gmail.com>
2019-10-26 05:46:10 -0400

    Add proper README GIF

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Simple little script that runs [`browser-sync`](https://www.browsersync.io/) & displays a QR code with the external URL.
 
+![BSQR desktop to iPhone QR scan usage GIF](https://media.giphy.com/media/iDxglQkeeLFUjINoR7/giphy.gif)
+
 # Installation
 
 To run once-in-a-while use:
--- index.js
+++ index.js
@@ -1,30 +1,19 @@
 #!/usr/bin/env node
 let qrcode = require('qrcode-terminal');
 let bs = require("browser-sync").create();
-let devip = require('dev-ip');
+let devIp = require('dev-ip');
 
-const commander = require('commander');
+// .init starts the server
+bs.init({
+    server: "."
+});
 
-const program = new commander.Command();
+/*
+ * NOTE: Here we're just adding a text QR code to prove it's possible
+ */
+qrcode.generate(`http://${devIp()[0]}:3000`, {small: true});
 
-program
-    .option('-p, --port <number>', 'port', 3000)
-    .option('-ns, --no-server', 'Stop the server from running')
-    .option('-s, --server <path>', 'Run a Local server (uses your cwd as the web root) [BrowserSync Option]', '.')
-    .option('-sm, --small', 'output extra debugging')
 
-program.parse(process.argv)
-
-let browserSyncOptions = {}
-if (program.noServer == undefined) {
-    browserSyncOptions
-        .server = (program.server == true) ? '.' : program.server
-}
-
-bs.init(browserSyncOptions)
-
-// qrcode.setErrorLevel('Q');
-const externalURL = `http://${devip()[0]}:${program.port}`
-qrcode.generate(externalURL, {small: true});
-
-bs.reload("*");
+// Now call methods on bs instead of the
+// main browserSync module export
+bs.reload("*"); // NOTE: Here I chose to reload everything`
